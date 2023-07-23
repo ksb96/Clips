1843f1f1de2c166e8b53387545743a038b745d04
ksb96 <kushalcr78017@gmail.com>
2023-07-23 13:29:56 +0530

    ffmpeg-D screenshot-upload --issue --init

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,11 +2,13 @@ import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid'
-import { last, switchMap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
+import { combineLatest, forkJoin } from "rxjs";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app'
 import { ClipService } from 'src/app/services/clip.service';
 import { Router } from '@angular/router';
+import { FfmpegService } from "src/app/services/ffmpeg.service";
 
 @Component({
   selector: 'app-upload',
@@ -25,6 +27,9 @@ export class UploadComponent implements OnDestroy {
   showPercentage = false
   user: firebase.User | null = null
   task?: AngularFireUploadTask
+  screenshots: string[] = []
+  selectedScreenshot = ''
+  screenshotTask?: AngularFireUploadTask
 
   title = new FormControl('', {
     validators: [
@@ -41,25 +46,32 @@ export class UploadComponent implements OnDestroy {
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
     private clipsService: ClipService,
-    private router: Router
-  ) { 
+    private router: Router,
+    public ffmpegService: FfmpegService
+  ) {
     auth.user.subscribe(user => this.user = user)
+    this.ffmpegService.init()
   }
 
   ngOnDestroy(): void {
     this.task?.cancel()
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
+    if (this.ffmpegService.isRunning) { return }
     this.isDragover = false
 
     this.file = ($event as DragEvent).dataTransfer ?
       ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
       ($event.target as HTMLInputElement).files?.item(0) ?? null
 
-    if(!this.file || this.file.type !== 'video/mp4') {
+    if (!this.file || this.file.type !== 'video/mp4') {
       return
     }
+
+    this.screenshots = await this.ffmpegService.getScreenshots(this.file)
+
+    this.selectedScreenshot = this.screenshots[0]
 
     this.title.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
@@ -67,7 +79,7 @@ export class UploadComponent implements OnDestroy {
     this.nextStep = true
   }
 
-  uploadFile() {
+  async uploadFile() {
     this.uploadForm.disable()
 
     this.showAlert = true
@@ -76,53 +88,81 @@ export class UploadComponent implements OnDestroy {
     this.inSubmission = true
     this.showPercentage = true
 
-    const clipFileName = uuid() 
+    const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
 
+    //screenshot store in firebase
+    const screenshotBlob = await this.ffmpegService.blobFromURL(
+      this.selectedScreenshot
+    )
+    const screenshotPath = `screenshots/$(clipFileName).png`
+
+    //upload video-clip/s
     this.task = this.storage.upload(clipPath, this.file)
     const clipRef = this.storage.ref(clipPath)
+    const screenshotRef = this.storage.ref(screenshotPath)
 
-    this.task.percentageChanges().subscribe(progress => {
-      this.percentage = progress as number / 100
-    })
+    //upload screenshot
+    this.screenshotTask = this.storage.upload(screenshotPath, screenshotBlob)
 
-    this.task.snapshotChanges().pipe(
-      last(),
-      switchMap(() => clipRef.getDownloadURL())
-    ).subscribe({
-      next: async (url) => {
-        const clip = {
-          uid: this.user?.uid as string,
-          displayName: this.user?.displayName as string,
-          title: this.title.value,
-          fileName: `${clipFileName}.mp4`,
-          url,
-          timestamp: firebase.firestore.FieldValue.serverTimestamp()
-        }
-
-        // const clipDocRef = await this.clipsService.createClip(clip)
-
-        console.log(clip)
-
-        this.alertColor = 'green'
-        this.alertMsg = 'Success! Your clip is now ready to share with the world.'
-        this.showPercentage = false
-
-        setTimeout(() => {
-          this.router.navigate([
-            // 'clip', clipDocRef.id
-          ])
-        }, 1000)
-      },
-      error: (error) => {
-        this.uploadForm.enable()
-
-        this.alertColor = 'red'
-        this.alertMsg = 'Upload failed! Please try again later.'
-        this.inSubmission = true
-        this.showPercentage = false
-        console.error(error)
+    //upload progress
+    combineLatest([
+      this.task.percentageChanges(),
+      this.screenshotTask.percentageChanges()
+    ]).subscribe((progress) => {
+      const [clipProgress, screenshotProgress] = progress
+      if (!clipProgress || !screenshotProgress) {
+        return
       }
+      const total = clipProgress + screenshotProgress
+      this.percentage = total as number / 200
     })
+
+    forkJoin([
+      this.task.snapshotChanges(),
+      this.screenshotTask.snapshotChanges()
+    ]).pipe(
+      switchMap(() => forkJoin([
+        clipRef.getDownloadURL(),
+        screenshotRef.getDownloadURL()
+      ]))).subscribe({
+        next: async (urls) => {
+          const [clipURL, screenshotURL] = urls
+
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url: clipURL,
+            screenshotURL,
+            screenshotFileName: `${clipFileName}.png`,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          }
+
+          // const clipDocRef = await this.clipsService.createClip(clip)
+
+          console.log(clip)
+
+          this.alertColor = 'green'
+          this.alertMsg = 'Success! Your clip is uploaded.'
+          this.showPercentage = false
+
+          setTimeout(() => {
+            this.router.navigate([
+              // 'clip', clipDocRef.id
+            ])
+          }, 1000)
+        },
+        error: (error) => {
+          this.uploadForm.enable()
+
+          this.alertColor = 'red'
+          this.alertMsg = 'Upload failed! Please try again later.'
+          this.inSubmission = true
+          this.showPercentage = false
+          console.error(error)
+        }
+      })
   }
 }
